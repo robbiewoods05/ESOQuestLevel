d69bd916ac8e6cd90ef8a14543f3f690d02fc858
Robert Woods <robertjawoods@gmail.com>
2016-01-13 15:21:17 +0000

    Added file with questIds, reworked logic

--- ESOQuestLevel.lua
+++ ESOQuestLevel.lua
@@ -2,12 +2,12 @@
 ESOQuestLevel = {}
 
 ESOQuestLevel.name = "ESOQuestLevel"
-questNames = {}
-questLevels = {}
+
+local names = require("QuestNames")
 
 -- Function to init addon
 function ESOQuestLevel:Initialize()
-    EVENT_MANAGER:RegisterForEvent(ESOQuestLevel.name, EVENT_QuestOffered, --[[ESOQuestLevel.FindQuestLevel(TODO: Find a way to work out the name of the quest the NPC is trying to give you)]] ESOQuestLevel.Yo)
+      EVENT_MANAGER:RegisterForEvent(ESOQuestLevel.name, EVENT_QUEST_OFFERED, --[[ESOQuestLevel.FindQuestLevel(TODO: Find a way to work out the name of the quest the NPC is trying to give you)]] ESOQuestLevel.Yo)
 end
 
 -- Event handler function for EVENT_ADD_ON_LOADED
@@ -15,59 +15,26 @@ function ESOQuestLevel.OnAddOnLoaded(event, addonName)
     if addonName == ESOQuestLevel.name then
         ESOQuestLevel:Initialize()
     end
-
-    local lineNumber = 0
-
-    --[[for line in io.lines("questlist.txt") do
-      questNames[lineNumber], questLevels[lineNumber]= string.match(line, "(%a+)(%d+)")
-      lineNumber = lineNumber + 1
-  end]]
 end
 
 
+--[[function ESOQuestLevel.Display(level)
+  TODO: Add code to display level
+end]]
 
---[[function ESOQuestLevel.Display(level)]]
---TODO: Add code to display level
-end
+function ESOQuestLevel.OnQuestOffered(event)
+  local questName = GetOfferedQuestShareInfo(names.GetQuestId(name))
+  local questLevel, questName
 
-function ESOQuestLevel.FindQuestLevel(event, name)
-    local playerLevel = GetUnitLevel("player")
+  for line in io.lines("questlist.txt") do
+    questName, questLevel = string.match(line, "([^%d]+)(%d+)")
 
-    local low = 1
-    local high = #questLevels
-    local mid = 0, startIndex = 0, endIndex = 0, countQuestsByLevel = 0, index = 0, level = 0
-
-    while low <= high do
-        mid = math.floor((low+high)/2)
-
-        if list[mid] > playerLevel then
-            high = mid - 1
-        else if list[mid] < playerLevel then
-            low = mid + 1
-        else
-             index = mid
-        end
+    if questNameById == questName then
+      break
     end
+  end
 
-    startIndex = index
-
-    while questLevels[index] == player do
-        countQuestsByLevel = countQuestsByLevel + 1
-        index = index + 1
-    end
-
-    endIndex = startIndex + countQuestsByLevel;
-
-    for i=startIndex,endIndex do
-        if questNames[i] == name then
-            level = questLevels[i]
-            break
-        else
-            level = 0
-        end
-    end
-
-    ESOQuestLevel.Display(level)
+    ESOQuestLevel.Display(questLevel)
 end
 
 -- Call OnAddOnLoaded function when EVENT_ADD_ON_LOADED occurs
